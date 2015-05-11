c2709e5ebfbd2e8f6f488cce04a818aff5d2bfbe
marbiru <uribram@gmail.com>
2015-05-11 11:15:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 // You can generally recognise jQuery by the use of a dollar sign before the first function name you see
 
 $(function(){
-  // because our index.html gave the button the id "action_button", we can select it using jQuery by writing "#action_button"
-  $("#action_button").click(function(){
-    $("#output").text(print_aphorism());
-  });
-  // prints an aphorism on mouse-click
-  $( "#refresh_button" ).trigger( "click" );
+  // this next line of code makes our website print the text "Pageload Dummy Text" as soon as the page is loaded 
+  // our index.html file named a certain paragraph with the id="output", therefore we can print things there using the selector "#output" in jQuery
+  $( "#output" ).text('Pageload Dummy Text');
+    // these next lines of code make our website do something when the button is clicked
+    // because our index.html gave the button the id "action_button", we can select it using jQuery by writing "#action_button"
+      $( "#action_button" ).click(function(){
+        $( "#output" ).text('Mouseclick Dummy Text');       
+      });
+});
